3af7b452562b3651c8663b8ca4a92edbe463caaa
Thuy <tpham045@hotmail.com>
2017-04-16 03:30:43 +0000

    Add files for heroku deploy

--- movie_trailers/models.py
+++ movie_trailers/models.py
@@ -10,15 +10,16 @@ class Movie(models.Model):
     story_line = models.TextField()
     poster_image_url = models.CharField(max_length=255)
     trailer_youtube_url = models.CharField(max_length=255)
+    # Add an editable youtube id field 
     trailer_youtube_id = models.TextField(editable=False)
     
     def __str__(self):
         return self.title
     
+    # Search for youtube id from youtube url and save to model
     def save(self):
         youtube_id_match = re.search(r'(?<=v=)[^&#]+', self.trailer_youtube_url)
         youtube_id_match = youtube_id_match or re.search(r'(?<=be/)[^&#]+', self.trailer_youtube_url)
         self.trailer_youtube_id = youtube_id_match.group(0) if youtube_id_match else None
         super(Movie, self).save()
-        # return self.trailer_youtube_id
         
